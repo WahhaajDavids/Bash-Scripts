cc95b0765bc7cb72871023589162ae7f203f8422
WahhaajDavids <wahhaaj.davids@mmiholdings.co.za>
2019-05-09 10:06:25 +0200

    added intellij reset script

--- IntellijReset.sh
+++ IntellijReset.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env bash
 
 echo "removeing evaluation key"
-rm ~/.IntelliJIdea15/config/eval/idea15.evaluation.key
+rm  -rf ~/.IntelliJIdea*/config/eval
+rm  -rf ~/.GoLand*/config/eval
+rm  -rf ~/.WebStorm*/config/eval
+
+rm -rf ~/.java/.userPrefs/jetbrains/goland
+rm -rf ~/.java/.userPrefs/jetbrains/idea
+rm -rf ~/.java/.userPrefs/jetbrains/webstorm
 
 echo "resetting evalsprt in options.xml"
-sed -i '/evlsprt/d' ~/.IntelliJIdea15/config/options/options.xml
+sed -i '/evlsprt/d' ~/.IntelliJIdea*/config/options/options.xml
+sed -i '/evlsprt/d' ~/.GoLand*/config/options/options.xml
+sed -i '/evlsprt/d' ~/.WebStorm*/config/options/options.xml
 
 echo "resetting evalsprt in prefs.xml"
 sed -i '/evlsprt/d' ~/.java/.userPrefs/prefs.xml
 
 
-## or:
-#For IntelliJ IDEA 2017.1.2 EAP on Windows 10, I had to:
-#
-#Go to ´~/.IntelliJIdea2017.1/config´
-#Remove the folder ´eval´
-#Edit file options/options.xml and remove all properties that the name begins with evlsprt, such as:
-#<property name="evlsprt3.171" value="18" />
-#Go to Windows Registry and under subkeys of HKCU\Software\JavaSoft\Prefs\jetbrains\idea delete keys that begin with evlsprt
+echo "change date file"
+find ~/.IntelliJIdea* -type d -exec touch -t $(date +"%Y%m%d%H%M") {} +;
+find ~/.IntelliJIdea* -type f -exec touch -t $(date +"%Y%m%d%H%M") {} +;
 
+find ~/.GoLand* -type d -exec touch -t $(date +"%Y%m%d%H%M") {} +;
+find ~/.GoLand* -type f -exec touch -t $(date +"%Y%m%d%H%M") {} +;
+
+find ~/.WebStorm* -type d -exec touch -t $(date +"%Y%m%d%H%M") {} +;
+find ~/.WebStorm* -type f -exec touch -t $(date +"%Y%m%d%H%M") {} +;
